f415e18a8c42fda1ac1e3a940b6d95e3b6ad8efd
alexandre-charlot_qonto <alexandre.charlot@qonto.com>
2025-07-26 15:11:57 +0200

    ƒix: some fix]

---
+++ debian-home.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, lib, nix-colors, nixvim, ... }:
+
+{
+  imports = [
+    nix-colors.homeManagerModules.default
+    nixvim.homeManagerModules.nixvim
+    (import ./nix/common/packages.nix {
+      inherit config pkgs lib;
+      fontConfig = {
+        family = "MesloLGL Nerd Font";
+        size = "15";
+        package = pkgs.nerd-fonts.meslo-lg;
+      };
+    })
+    (import ./nix/linux/packages.nix { inherit config pkgs lib; })
+    (import ./nix/linux/services.nix { inherit config pkgs lib; })
+  ];
+
+  home = {
+    username = "alexandre";
+    homeDirectory = "/home/alexandre";
+    stateVersion = "23.11";
+  };
+
+  # Schéma de couleurs
+  colorScheme = nix-colors.colorSchemes.catppuccin-frappe;
+
+  fonts = { fontconfig.enable = true; };
+
+  home.sessionVariables = {
+    PATH = "$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH";
+  };
+
+  programs.home-manager.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+}
