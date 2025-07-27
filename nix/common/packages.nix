7d56ca752b0d436e5ad0b767ef0f525b32c473e9
alexandre-charlot_qonto <alexandre.charlot@qonto.com>
2025-07-27 22:43:21 +0200

    feat: wsl stuff

--- nix/common/packages.nix
+++ nix/common/packages.nix
@@ -6,7 +6,8 @@ with pkgs;
 let
   # Import des configurations
   fishConfig = import ../configs/fish/config.nix { inherit config; };
-  vscodeConfig = import ../configs/vscode/config.nix { inherit config pkgs lib; };
+  vscodeConfig =
+    import ../configs/vscode/config.nix { inherit config pkgs lib; };
   kittyConfig = import ../configs/kitty.nix { inherit config fontConfig; };
   nixvimConfig = import ../configs/vim/config.nix { inherit config pkgs lib; };
   gitConfig = import ../configs/git.nix;
@@ -44,51 +45,40 @@ let
     nil
   ];
 
-  commandLineTools = [ 
-    autojump
-    btop 
-    eza 
-    gtop 
-    neofetch 
-    nix-tree 
-  ];
+  commandLineTools = [ autojump btop eza gtop neofetch nix-tree ];
 
   node = nodejs_20;
 
-  web = [
-    heroku
-    netlify-cli
-    nodePackages.http-server
-  ];
+  web = [ heroku netlify-cli nodePackages.http-server ];
 
   # Pas de packages spécifiques ici - ils sont dans darwin/ et linux/
 
 in {
-  home.packages = development 
-    ++ lsp 
-    ++ commandLineTools 
-    ++ web 
-    ++ [ 
-      node 
-      fzf
-      ripgrep
-      bat
-      mktemp
-      tree
-      unrar
-      firefox-bin
-    ];
+  home.packages = development ++ lsp ++ commandLineTools ++ web ++ [
+    node
+    fzf
+    ripgrep
+    bat
+    mktemp
+    tree
+    unrar
+    firefox-bin
+    alacritty
+    fontConfig.package
+  ];
 
+  # Alias pour utiliser la version système
+  # home.shellAliases = { kitty = "/usr/bin/kitty"; };
   # Configuration commune
   programs = {
     fish = fishConfig // { enable = true; };
-    
+
     autojump = {
       enable = true;
       enableFishIntegration = true;
     };
     git = gitConfig // { enable = true; };
-    
+
     vscode = vscodeConfig // { enable = true; };
 
     starship = {
@@ -96,10 +86,9 @@ in {
       enableFishIntegration = true;
     };
 
-    kitty = kittyConfig // { enable = true; };
-
+    # kitty = kittyConfig // { enable = true; };
     nixvim = nixvimConfig // { enable = true; };
-    
+
     direnv = {
       enable = true;
       nix-direnv.enable = true;
